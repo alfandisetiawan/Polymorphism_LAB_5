8d6d4319ed45f8c12ae713e476b20e90d2c55e2b
alfandisetiawan <alfandi.2713@students.amikom.ac.id>
2020-05-07 14:48:45 +0700

    tugas LAB 5

---
+++ Polymorphism.1/canon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism._1
+{
+    class canon : printerwindows
+    {
+        public override void show()
+        {
+            Console.WriteLine("canon display dimension : 9.5*12");
+        }
+        public override void print()
+        {
+            Console.WriteLine("canon printer printing.....");
+        }
+    }
+}
---
+++ Polymorphism.1/epson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism._1
+{
+   class Epson : printerwindows
+
+    {
+        public override void show()
+        {
+            Console.WriteLine("epson display dimension : 10*11");
+        }
+        public override void print()
+        {
+            Console.WriteLine("epson printer printing.....");
+        }
+    }
+}
